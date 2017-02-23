b11e186c651d48a2cb4f0bb4bdb4eeef54042c09
Vuk Stanković <vuks89@gmail.com>
2017-02-23 20:12:19 +0100

    Added initial Codeigniter with Eloquent ORM

---
+++ application/models/User.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vuk
+ * Date: 23.2.17.
+ * Time: 19.59
+ */
+
+use \Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent {
+
+}
