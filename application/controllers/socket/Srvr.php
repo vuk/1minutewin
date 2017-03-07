e8cb997949f22bd76ac390a0b1140455ed85f9a8
Vuk Stanković <vuks89@gmail.com>
2017-03-07 22:17:05 +0100

    Logging socket server start

--- application/controllers/socket/Srvr.php
+++ application/controllers/socket/Srvr.php
@@ -5,18 +5,22 @@ use Ratchet\WebSocket\WsServer;
 use Ratchet\Http\HttpServer;
 class Srvr extends CI_Controller {
 
+    protected $server = null;
+
     public function start()
     {
-        $server = IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    new SockServer()
-                )
-            ),
-            8080
-        );
+        if ($this->server === null) {
+            $this->server = IoServer::factory(
+                new HttpServer(
+                    new WsServer(
+                        new SockServer()
+                    )
+                ),
+                8080
+            );
 
-        echo "Socket server is running on port 8080. Started at " . date('Y-m-d H:i:s', strtotime('now'))."\n";
-        $server->run();
+            echo "Socket server is running on port 8080. Started at " . date('Y-m-d H:i:s', strtotime('now'))."\n";
+            $this->server->run();
+        }
     }
 }
