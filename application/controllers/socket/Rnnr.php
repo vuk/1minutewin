c5da8d14b6800ee75fb5f4efdcb84050a55976c9
Vuk Stanković <vuks89@gmail.com>
2017-03-09 21:38:13 +0100

    Making sure only one instance is running

--- application/controllers/socket/Rnnr.php
+++ application/controllers/socket/Rnnr.php
@@ -1,17 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Rnnr extends CI_Controller {
 
     protected $runner = null;
+    protected $socket = null;
 
     public function start()
     {
         if ($this->runner === null) {
             $this->runner = Runner::getInstance();
+            try {
+                $this->socket = socket_create_listen(8081);
 
-            echo "Runner is started at " . date('Y-m-d H:i:s', strtotime('now'))."\n";
-            $this->runner->run();
+                echo "Runner is started at " . date('Y-m-d H:i:s', strtotime('now'))."\n";
+                $this->runner->run();
+            } catch (Exception $e) {
+                echo "Runner is working ok \n";
+            }
         }
     }
 }
