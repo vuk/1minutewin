292b8d68580876fc5493c705f9ffabe83617fb95
Vuk Stanković <vuks89@gmail.com>
2017-03-16 23:05:06 +0100

    Structure for two child processes within socket server

---
+++ application/controllers/socket/Bidder.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Bidder extends CI_Controller {
+
+
+    public function start () {
+        while(true) {
+            echo "Bid";
+            sleep(5);
+        }
+    }
+
+}
--- application/controllers/socket/Srvr.php
+++ application/controllers/socket/Srvr.php
@@ -1,40 +1,68 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-use Ratchet\Server\IoServer;
-use Ratchet\WebSocket\WsServer;
-use Ratchet\Http\HttpServer;
+
 class Srvr extends CI_Controller {
+    protected $clients;
+    protected $runner;
+    protected $order;
+    private static $instance = null;
 
-    protected $server = null;
+    public function __construct() {
+        parent::__construct();
+        $this->clients = new \SplObjectStorage();
+    }
 
-    public function start()
-    {
-        try {
-            $connection = @fsockopen('localhost', 8080);
+    public function start () {
+        $loop = React\EventLoop\Factory::create();
+        $socket = new React\Socket\Server($loop);
 
-            if (!is_resource($connection))
-            {
-                if ($this->server === null) {
-                    $this->server = IoServer::factory(
-                        new HttpServer(
-                            new WsServer(
-                                SockServer::getInstance()
-                            )
-                        ),
-                        8080
-                    );
+        // START RUNNER SERVICE
+        $runner = new React\ChildProcess\Process('php index.php rnnr start');
 
-                    echo "Socket server is running on port 8080. Started at " . date('Y-m-d H:i:s', strtotime('now'))."\n";
-                    Runner::slack("Socket server is running on port 8080. Started at " . date('Y-m-d H:i:s', strtotime('now'))."\n", "socket");
-                    $this->server->run();
-                } else {
-                    log_message('debug', "Server is running ok...");
+        $runner->on('exit', function($exitCode, $termSignal) {
+            echo "Runner process stopped";
+        });
+
+        $loop->addTimer(0.001, function($timer) use ($runner) {
+            $runner->start($timer->getLoop());
+            $runner->stdout->on('data', function($output) {
+                echo "Child script says: {$output}";
+            });
+        });
+        // END START RUNNER SERVICE
+
+        // START BIDDER SERVICE
+        $bidder = new React\ChildProcess\Process('php index.php bidder start');
+
+        $bidder->on('exit', function($exitCode, $termSignal) {
+            echo "Runner process stopped";
+        });
+
+        $loop->addTimer(0.001, function($timer) use ($bidder) {
+            $bidder->start($timer->getLoop());
+            $bidder->stdout->on('data', function($output) {
+                echo "Child script says: {$output}";
+            });
+        });
+        // END START BIDDER SERVICE
+
+        $socket->on('connection', function ($conn) {
+            $this->clients->attach($conn);
+            $conn->on('data', function ($data) use ($conn) {
+                foreach ($this->clients as $current) {
+                    if ($conn === $current) {
+                        continue;
+                    }
+                    $current->write($conn->getRemoteAddress().': ');
+                    $current->write($data);
                 }
-            } else {
-                fclose($connection);
-            }
-        } catch (\Exception $e) {
-            //echo "Socket server is already started"."\n";
-        }
+            });
+            $conn->on('end', function () use ($conn) {
+                $this->clients->detach($conn);
+            });
+        });
+        echo "Socket server listening on port 8080.\n";
+        $socket->listen(8080);
+        $loop->run();
     }
 }
