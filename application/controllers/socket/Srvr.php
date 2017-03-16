81ec3a5d31e7183d7dd9e9f7b69a3eaa2463e291
Vuk Stanković <vuks89@gmail.com>
2017-03-16 23:14:05 +0100

    Structure for two child processes within socket server

--- application/controllers/socket/Srvr.php
+++ application/controllers/socket/Srvr.php
@@ -12,56 +12,60 @@ class Srvr extends CI_Controller {
     }
 
     public function start () {
-        $loop = React\EventLoop\Factory::create();
-        $socket = new React\Socket\Server($loop);
+        try {
+            $loop = React\EventLoop\Factory::create();
+            $socket = new React\Socket\Server($loop);
 
-        // START RUNNER SERVICE
-        /*$runner = new React\ChildProcess\Process('php index.php rnnr start');
+            // START RUNNER SERVICE
+            /*$runner = new React\ChildProcess\Process('php index.php rnnr start');
 
-        $runner->on('exit', function($exitCode, $termSignal) {
-            echo "Runner process stopped";
-        });
-
-        $loop->addTimer(0.001, function($timer) use ($runner) {
-            $runner->start($timer->getLoop());
-            $runner->stdout->on('data', function($output) {
-                echo "Child script says: {$output}";
+            $runner->on('exit', function($exitCode, $termSignal) {
+                echo "Runner process stopped";
             });
-        });*/
-        // END START RUNNER SERVICE
 
-        // START BIDDER SERVICE
-        /*$bidder = new React\ChildProcess\Process('php index.php bidder start');
+            $loop->addTimer(0.001, function($timer) use ($runner) {
+                $runner->start($timer->getLoop());
+                $runner->stdout->on('data', function($output) {
+                    echo "Child script says: {$output}";
+                });
+            });*/
+            // END START RUNNER SERVICE
 
-        $bidder->on('exit', function($exitCode, $termSignal) {
-            echo "Runner process stopped";
-        });
+            // START BIDDER SERVICE
+            /*$bidder = new React\ChildProcess\Process('php index.php bidder start');
 
-        $loop->addTimer(0.001, function($timer) use ($bidder) {
-            $bidder->start($timer->getLoop());
-            $bidder->stdout->on('data', function($output) {
-                echo "Child script says: {$output}";
+            $bidder->on('exit', function($exitCode, $termSignal) {
+                echo "Runner process stopped";
             });
-        });*/
-        // END START BIDDER SERVICE
 
-        $socket->on('connection', function ($conn) {
-            $this->clients->attach($conn);
-            $conn->on('data', function ($data) use ($conn) {
-                foreach ($this->clients as $current) {
-                    if ($conn === $current) {
-                        continue;
+            $loop->addTimer(0.001, function($timer) use ($bidder) {
+                $bidder->start($timer->getLoop());
+                $bidder->stdout->on('data', function($output) {
+                    echo "Child script says: {$output}";
+                });
+            });*/
+            // END START BIDDER SERVICE
+
+            $socket->on('connection', function ($conn) {
+                $this->clients->attach($conn);
+                $conn->on('data', function ($data) use ($conn) {
+                    foreach ($this->clients as $current) {
+                        if ($conn === $current) {
+                            continue;
+                        }
+                        $current->write($conn->getRemoteAddress().': ');
+                        $current->write($data);
                     }
-                    $current->write($conn->getRemoteAddress().': ');
-                    $current->write($data);
-                }
+                });
+                $conn->on('end', function () use ($conn) {
+                    $this->clients->detach($conn);
+                });
             });
-            $conn->on('end', function () use ($conn) {
-                $this->clients->detach($conn);
-            });
-        });
-        echo "Socket server listening on port 8080.\n";
-        $socket->listen(8080);
-        $loop->run();
+            echo "Socket server listening on port 8080.\n";
+            $socket->listen(8080);
+            $loop->run();
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
     }
 }
