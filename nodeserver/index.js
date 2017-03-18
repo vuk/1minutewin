ca7814faacbb2220a1c69f28e0d445b854e504f2
Vuk Stanković <vuks89@gmail.com>
2017-03-18 16:44:39 +0100

    Nodejs server added

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -2,7 +2,7 @@
 (function () {
     const exec = require('child_process').exec;
 
-    const runner = exec('php ../index.php socket/rnnr start', function(err, stdout, stderr){
+    const runner = exec('php -f ../index.php socket/rnnr start', function(err, stdout, stderr){
         if(err || stderr){
             console.error(err || stderr);
             return;
