c03d8cc9812b3b4c0776268b06df70891565a6cd
Vuk Stanković <vuks89@gmail.com>
2017-03-22 00:07:25 +0100

    Added heartbeat on focus

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -94,16 +94,15 @@
 
     function heartbeat(payload) {
         console.log(payload);
-        if (currentOrder)
-        var t1 = new Date(parsed.order.updated_at);
-        var t2 = new Date(parsed.order.ending_at);
-        var t3 = new Date();
-        t3.setHours(t3.getHours() - 7);
-        parsed.order.duration = Math.floor(t2.getTime()) - Math.floor(t1.getTime());
-        parsed.order.durationLeft = Math.floor(t2.getTime()) - Math.floor(t3.getTime());
-        socket.emit('order', parsed);
-        currentOrder = parsed.order;
-
+        if (currentOrder) {
+            var t1 = new Date(currentOrder.updated_at);
+            var t2 = new Date(currentOrder.ending_at);
+            var t3 = new Date();
+            t3.setHours(t3.getHours() - 7);
+            currentOrder.duration = Math.floor(t2.getTime()) - Math.floor(t1.getTime());
+            currentOrder.durationLeft = Math.floor(t2.getTime()) - Math.floor(t3.getTime());
+            socket.emit('order', {message: 'new order', order: currentOrder});
+        }
     }
 
 })();
