62df8ba176247680bc84469edad139a23ce5a8a1
Vuk Stanković <vuks89@gmail.com>
2017-03-21 01:30:56 +0100

    Live duration left recalculation

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -76,7 +76,7 @@
                     var t1 = new Date(parsed.order.updated_at);
                     var t2 = new Date(parsed.order.ending_at);
                     var t3 = new Date();
-                    t3.setHours(t3.getHours() - 7);
+                    //t3.setHours(t3.getHours() - 7);
                     parsed.order.duration = Math.floor(t2.getTime()) - Math.floor(t1.getTime());
                     parsed.order.durationLeft = Math.floor(t2.getTime()) - Math.floor(t3.getTime());
                     socket.emit('order', parsed);
