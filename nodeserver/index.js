eb39767a7321c2b064677fa01692ab39903f06a8
Vuk Stanković <vuks89@gmail.com>
2017-03-21 01:20:45 +0100

    Live duration left recalculation

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -52,6 +52,7 @@
             var t1 = new Date(currentOrder.updated_at);
             var t2 = new Date(currentOrder.ending_at);
             var t3 = new Date();
+            console.log(t3.getUTCDate());
             t3.setHours(t3.getHours() - 7);
             currentOrder.duration = Math.floor(t2.getTime()) - Math.floor(t1.getTime());
             currentOrder.durationLeft = Math.floor(t2.getTime()) - Math.floor(t3.getTime());
